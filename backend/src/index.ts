7d7a12bda2a30f57b11bb905595a4d035a0c4d59
Henrique Dummer <hatiehique2005@gmail.com>
2026-05-07 17:13:47 -0300

    Return real data trough the websocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,27 @@
-import WebSocket = require("ws")
+import { WebSocketServer } from "ws"  
 
-const wss = new WebSocket.WebSocketServer({
+const wss = new WebSocketServer({
   port: 3001,
 })
 
 wss.on("connection", (ws) => {
   console.log("Client connected")
-
-  ws.send(
-    JSON.stringify({
-      message: "Connected to weather socket",
-    })
-  )
 })
+
+async function broadcastWeather() {
+  const response = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=-29.46&longitude=-51.96&current=temperature_2m,relative_humidity_2m,wind_speed_10m"
+  )
+
+  const data = await response.json()
+
+  wss.clients.forEach((client) => {
+    client.send(JSON.stringify(data))
+  })
+
+  console.log(data)
+}
+
+setInterval(broadcastWeather, 1000 * 60)
 
 console.log("WebSocket server running on port 3001")
